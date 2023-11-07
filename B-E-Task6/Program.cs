f2b50971941f61bffca0cb2061c06b227156f3ee
suleymanibishov <suleyman22022@gmail.com>
2023-11-08 01:31:02 +0400

    Task

---
+++ B-E-Task6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace B_E_Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st = new Student();
+            st.Grades = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            foreach (int i in st.Grades)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ B-E-Task6/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_E_Task6
+{
+    class Student
+    {
+        public string FullName { get; set; }
+        public string Class { get; set; }
+        int[] grades;
+        public int[] Grades { get => grades; 
+            set {
+                int index = 0;
+                foreach (int i in value)
+                {
+                    if (i % 2 == 0)
+                    {
+                        index++;
+                    }
+                }
+                grades = new int[index];
+                index = 0;
+                foreach (int i in value)
+                {
+                    if (i % 2 == 0)
+                    {
+                        grades[index++] = i;
+                    }
+                }
+            } 
+        }
+    }
+}
